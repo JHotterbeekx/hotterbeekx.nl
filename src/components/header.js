6c2d9f13ba37e2af99310d45faf4f4ad0f0bda80
John Hotterbeekx <john.online@hotterbeekx.nl>
2019-04-17 21:27:53 +0200

    First iteration of projects page

--- src/components/header.js
+++ src/components/header.js
@@ -3,38 +3,56 @@ import { Link } from 'gatsby'
 import Logo from '../images/logo.png';
 
 const Header = ({ siteTitle, subTitle }) => (
-  <div
-    style={{
-      background: '#000000',
-      marginBottom: '1.45rem',
-      position: 'sticky',
-      top: 0,
-      zIndex: 9999,
-      borderBottom: '1px solid white',
-    }}
-  >
+  <div style={{
+    background: '#000000',
+    marginBottom: '20px',
+    position: 'sticky',
+    top: 0,
+    zIndex: 9999,
+    borderBottom: '1px solid white',
+  }}>
+  
     <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 1060,
-        padding: '0.25rem 0rem',
-        textAlign: 'left',
-      }}
+      
     >
-      <div style={{ fontWeight: 'bold', fontFamily: '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif' }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-            verticalAlign: 'middle'
-          }}
-        >
-          <img className="logo" src={Logo} alt="logo" style={{  }} />
-          <span className="title">{siteTitle}</span>
-          <span className="sub-title">{subTitle}</span>
-        </Link>
-        
+      <div
+        style={{
+          margin: '0 auto',
+          maxWidth: 1060,
+          padding: '0.25rem 0rem 0rem',
+          textAlign: 'left',
+        }}
+      >
+        <div style={{ fontWeight: 'bold', fontFamily: '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif' }}>
+          <Link
+            to="/"
+            style={{
+              color: 'white',
+              textDecoration: 'none',
+              verticalAlign: 'middle'
+            }}
+          >
+            <img className="logo" src={Logo} alt="logo" style={{  }} />
+            <span className="title">{siteTitle}</span>
+            <span className="sub-title">{subTitle}</span>
+          </Link>
+          
+        </div>
+      </div>
+    </div>
+    <div
+    >
+      <div
+        style={{
+          margin: '0 auto',
+          maxWidth: 1060,
+          textAlign: 'left',
+        }}
+      >
+        <div className="header-menu">
+          <Link to="/" className="header-menu-link">Blog</Link>&nbsp;|&nbsp; 
+          <Link to="/projects" className="header-menu-link">Projects</Link>
+        </div>
       </div>
     </div>
   </div>
---
+++ src/pages/projects.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+const projects = [
+  {
+    title: 'Hotterbeekx.nl',
+    description: 'My personal website, with my articles and project scribbles.',
+    source: 'https://github.com/JHotterbeekx/hotterbeekx.nl',
+    demo: 'https://www.hotterbeekx.nl',
+  },
+ {
+   title: 'Word-search-solver',
+   description: 'NPM package that allows you to solve word search puzzles.',
+   source: 'https://github.com/JHotterbeekx/word-search-solver',
+   demo: undefined,
+ },
+ {
+   title: 'Selenium Extensions',
+   description: 'NuGet package with extension methods to make testing in Selenium easier.',
+   source: 'https://github.com/JHotterbeekx/selenium-extensions',
+   demo: undefined,
+ },
+ {
+   title: 'Rating Image',
+   description: 'Web component allowing you to render a customized rating image.',
+   source: 'https://github.com/JHotterbeekx/rating-image',
+   demo: undefined,
+ },
+ {
+   title: 'Catify',
+   description: 'Chrome extension that replaces all images by images of cats.',
+   source: 'https://github.com/JHotterbeekx/CatifyChromeExtension',
+   demo: 'https://chrome.google.com/webstore/detail/catify/ihjgbjdkgnploojjhjepdaigkoigianp',
+ },
+ {
+   title: 'React-dice-roller',
+   description: 'NPM package that gives you a react component to roll a customizable dice in your browser.',
+   source: 'https://github.com/JHotterbeekx/react-dice',
+   demo: 'http://dice.hotterbeekx.nl',
+ },
+ {
+   title: 'Online dice tool',
+   description: 'A website that allows you to play your favorite games when you don\'t have any actual dice.',
+   source: 'https://github.com/JHotterbeekx/online-dice-tool',
+   demo: 'http://dice.hotterbeekx.nl',
+ }
+]
+
+function GetLinks(project) {
+  const result = [];
+  if (project.demo) result.push(<a className="card-link-demo" target="_blank" href={project.demo}>Demo</a>);
+  if (project.source) result.push(<a className="card-link-source" target="_blank" href={project.source}>Source</a>);
+  return result;
+}
+
+const ProjectPage = () => {
+  return (
+    <Layout>
+      {projects.map(project => (
+        <div className="card">
+          <div className="card-title">{project.title}</div>
+          <div className="card-description">{project.description}</div>
+          <div className="card-links">
+            {GetLinks(project)}            
+          </div>
+        </div>
+      ))}
+    </Layout>
+  )
+}
+
+export default ProjectPage;
